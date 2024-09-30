71f953c3c95bab1ae5f71e94d3dbc8f0b051b554
Belov-AI <Belov.Alexander@urfu.ru>
2024-09-30 15:31:56 +0500

    Символы

---
+++ CharsAndStrings/CharsAndStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharsAndStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char letter = 'z';
+            Console.WriteLine(letter);
+
+            letter = (char)0x42F;
+            Console.WriteLine(letter);
+
+            letter = char.ToLower(letter);
+            Console.WriteLine(letter);
+            Console.WriteLine(char.IsLetter(letter));
+            Console.WriteLine(char.IsDigit(letter));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CharsAndStrings/CharsAndStrings/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("CharsAndStrings")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CharsAndStrings")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("b4f4c25b-d548-4e3c-a735-ae94981e1e3d")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
