1957a32611b5628a9c2c1653c673a874ca44ba0f
Belov-AI <Belov.Alexander@urfu.ru>
2024-10-07 16:21:38 +0500

    Добавлена работа с CultureInfo

--- CharsAndStrings/CharsAndStrings/Program.cs
+++ CharsAndStrings/CharsAndStrings/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -38,7 +39,42 @@ namespace CharsAndStrings
             Console.WriteLine("Роман Л. Н. Толстого \x00ABВойна и мир\x00BB");
             Console.WriteLine("Прямой угол равен 90\x00B0\a");
 
+
+            double x = 551.23456789;
+            int y = 2;
+            Console.WriteLine("x = {0,6:F4} y = {1:G}", x, y);
+            Console.WriteLine($"абсцисса {x,6:F4}; ордината {y}");
+            Console.WriteLine($"точка ({x:F2}; {y:F2})");
+
+            Console.WriteLine("x = {0,6:E}", x);
+
+            Console.WriteLine("Сейчас {0:HH:mm dd.MM.yy}", DateTime.Now);
+
+            Console.WriteLine();
+            PrintCultureInfo(CultureInfo.CurrentCulture.Name);
+            PrintCultureInfo("en-US");
+            PrintCultureInfo("en-GB");
+            PrintCultureInfo("fr-FR");
+            PrintCultureInfo("es-ES");
+
             Console.ReadKey();
         }
+
+        static void PrintCultureInfo(string name)
+        {
+            var oldCulture = CultureInfo.CurrentCulture;
+            CultureInfo.CurrentCulture = new CultureInfo(name);
+
+            Console.WriteLine(CultureInfo.CurrentCulture.Name);
+            var x = 1.234556;
+            Console.WriteLine(x);
+            var dt = DateTime.Now;
+            Console.WriteLine(dt.ToLongDateString());
+            Console.WriteLine(dt.ToShortDateString());
+            Console.WriteLine();
+
+            CultureInfo.CurrentCulture = oldCulture;
+        }
+
     }
 }
