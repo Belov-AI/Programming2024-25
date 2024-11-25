9742e9c6cdfbb19f7e39942da4fabcbc40642fe0
Belov-AI <Belov.Alexander@urfu.ru>
2024-11-25 16:02:48 +0500

    Классы Person и User

---
+++ Classes/Classes/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+        public string LastName;
+        private int age;
+
+        public void SetAge(int age)
+        {
+            if (age < 0 || age > 150)
+                throw new ArgumentException();
+            
+            this.age = age;
+        }
+        public void PrintInfo() 
+        {
+            Console.WriteLine($"{Name} {LastName}, возраст: {age}");
+        }
+    }
+}
---
+++ Classes/Classes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person mike;
+            mike = new Person();
+            mike.Name = "Михаил";
+            mike.LastName = "Иванов";
+            mike.SetAge(20);
+
+            var kate = new Person() 
+            {
+                Name = "Екатерина", 
+                LastName = "Петрова", 
+            };
+
+            kate.SetAge(19);
+
+            mike.PrintInfo();
+            kate.PrintInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
