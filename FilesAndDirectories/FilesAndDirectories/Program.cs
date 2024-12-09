d186e1e5233025de396511b5f3eb45b307a1a813
Belov-AI <Belov.Alexander@urfu.ru>
2024-12-09 16:01:17 +0500

    Создание и удаление файлов

---
+++ FilesAndDirectories/FilesAndDirectories/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FilesAndDirectories
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("test.txt")) 
+            {
+                File.Create("test.txt");
+
+            }
+
+            Console.WriteLine(File.Exists("test.txt"));
+
+            File.Delete("test.txt");
+            Console.WriteLine(File.Exists("test.txt"));
+
+            Console.ReadKey();
+        }
+    }
+}
