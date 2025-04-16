6d92e9e26978b566faacb5296540d65c204b8366
Belov-AI <Belov.Alexander@urfu.ru>
2025-04-16 19:23:27 +0500

    Словари: футбол

---
+++ FootballChampionships/FootballChampionships/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballChampionships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var champions = new Dictionary<string, int>();
+
+            using (var file = new StreamReader("football.txt", Encoding.Default))
+            {
+                while (!file.EndOfStream)
+                {
+                    var team = file.ReadLine().Split('\t')[1];
+                    
+                    if(champions.ContainsKey(team))
+                        champions[team]++;
+                    else 
+                        champions[team] = 1;
+                }
+            }
+
+            PrintDictionary(champions);
+        }
+
+        static void PrintDictionary(Dictionary<string, int> dict)
+        {
+            foreach(var item in dict)
+                Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+
+        static void SortDictionaryByValuesDescending(Dictionary<string, int> dict)
+        {
+            
+        }
+    }
+}
