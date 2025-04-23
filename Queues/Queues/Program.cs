2b7613829f6b94477454d76d542aec0faab277bb
Belov-AI <Belov.Alexander@urfu.ru>
2025-04-23 19:23:00 +0500

    Stacks

---
+++ Queues/Queues/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите колочество чисел");
+
+            var n = int.Parse(Console.ReadLine());
+
+            var numbers = new Queue<int>[10];
+
+            for(var i =0; i < numbers.Length; i++)
+                numbers[i] = new Queue<int>();
+
+            var rnd = new Random();
+
+
+            
+        }
+    }
+}
---
+++ Stacks/Stacks/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class Program
+    {
+        static Dictionary<char, char> brackets = new Dictionary<char, char>();
+        // ключ - закрыв. скобка, значение - открыв. скобка
+        static void Main(string[] args)
+        {
+            brackets[')'] = '(';
+            brackets[']'] = '[';
+            brackets['»'] = '«';
+
+            while (true) 
+            {
+                Console.WriteLine("Введите выражение со скобками. Enter - выход");
+                var input = Console.ReadLine();
+
+                if (input.Length == 0) break;
+
+                if (IsExpessionCorrect(input))
+                    Console.WriteLine("Скобки расставлены верно");
+                else
+                    Console.WriteLine("Скобки расставлены неверно");
+
+                Console.WriteLine();
+
+            }
+        }
+
+        static bool IsExpessionCorrect(string expression)
+        {
+            var stack = new Stack<char>();
+
+            foreach (char c in expression)
+                if (brackets.ContainsValue(c))
+                    stack.Push(c);
+                else if (brackets.ContainsKey(c) &&
+                    (stack.Count == 0 || stack.Pop() != brackets[c]))
+                    return false;
+
+            return stack.Count == 0;
+        }
+    }
+}
